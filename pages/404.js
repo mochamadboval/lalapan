600dd2396196267b2075141286bc6ba09894d000
Mochamad Boval <mochamadboval@gmail.com>
2024-01-16 07:36:41 +0700

    Laman 404

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <article className="py-32 text-center">
+      <h2 className="text-4xl font-semibold leading-tight">404</h2>
+      <p className="mt-2">Laman tidak ditemukan.</p>
+    </article>
+  );
+}
